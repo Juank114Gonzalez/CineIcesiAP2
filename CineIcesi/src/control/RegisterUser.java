a89de1f30fff1b89bfce7c7d991ddd7a2914e4ce
Juan Camilo Gonzalez <93550730+Juank114Gonzalez@users.noreply.github.com>
2022-02-23 09:52:22 -0500

    Commit con el registro de usuario implementado
    Faltan validaciones para añadir al nuevo usuario.
    Manejar excepciones para controlar las alertas.
    Falta documentación de todo el código.

--- CineIcesi/src/control/RegisterUser.java
+++ CineIcesi/src/control/RegisterUser.java
@@ -4,12 +4,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.Optional;
 import java.util.ResourceBundle;
-
-import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
@@ -23,8 +22,9 @@ import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import main.Main;
 import model.User;
+import model.UserData;
 
-public class RegisterUser extends Application implements Initializable{
+public class RegisterUser implements Initializable{
 
 	//Attributes
 	private User genericUser;
@@ -66,42 +66,61 @@ public class RegisterUser extends Application implements Initializable{
     
 
     @FXML
-    void pressBTN(ActionEvent event) {
-    	if(event.getSource() == registerBTN) {
-    		genericUser = new User(nameRegisterTF.getText(), idTF.getText(),passwordTF.getText());
-    		Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-    		alert.setTitle("Alert!");
-            alert.setContentText("This is an alert");
+    void registerUser(ActionEvent event) {
+    	boolean flag = false;
+    	if(nameRegisterTF.getText().equals("") || idTF.getText().equals("") || passwordTF.getText().equals("") || nameRegisterTF.getText().equals(" ") || idTF.getText().equals(" ") || passwordTF.getText().equals(" ")) {
+			Alert alert = new Alert(Alert.AlertType.ERROR);
+    		alert.setTitle("Error al añadir!");
+            alert.setContentText("Ha ocurido un error al momento de añadir al usuario");
             Optional<ButtonType> result = alert.showAndWait();
-
-    		launch();
-    	}else {}
-    }
+		}else {
+			genericUser = new User(nameRegisterTF.getText(), idTF.getText(),passwordTF.getText());
+			if(!UserData.data.contains(genericUser)) {
+				UserData.data.add(genericUser);
+				flag = true;
+			}
+			//Usar serialización para validar en la dataBase
+    		if(flag) {
+    			Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        		alert.setTitle("Usuario añadido!");
+                alert.setContentText("El usuario "+ nameRegisterTF.getText() + " ha sido añadido con éxito");
+                Optional<ButtonType> result = alert.showAndWait();
+                try {
+        			FXMLLoader loader = new FXMLLoader(Main.class.getResource("../ui/MainWindow.fxml"));
+        			Parent parent = (Parent) loader.load();
+        			
+        			Stage stage = new Stage();
+        			
+        			Scene scene = new Scene(parent);
+        			
+        			stage.setScene(scene);
+        			
+        			stage.show();
+        			
+        			try {
+        	    		Node source = (Node)event.getSource();
+        	    		Stage old = (Stage) source.getScene().getWindow();
+        	    		old.close();
+        	    	}catch(Exception e){
+        	    		e.printStackTrace();
+        	    	}
+        		} catch (IOException e) {
+        			// TODO: handle exception
+        			e.printStackTrace();
+        		}
+        		
+    		}else {
+    			Alert alert = new Alert(Alert.AlertType.WARNING);
+        		alert.setTitle("Usuario Existente!");
+                alert.setContentText("Está intentando añadir un usuario existente");
+                Optional<ButtonType> result = alert.showAndWait();
+    		}
+		}
+	}
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		// TODO Auto-generated method stub
 		
 	}
-
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		// TODO Auto-generated method stub
-		try {
-			FXMLLoader loader = new FXMLLoader(Main.class.getResource("MainWindow.fxml"));
-			Parent parent = (Parent) loader.load();
-			
-			Stage stage = new Stage();
-			
-			Scene scene = new Scene(parent);
-			
-			stage.setScene(scene);
-			
-			stage.show();
-		} catch (IOException e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-	}
-	
 }
